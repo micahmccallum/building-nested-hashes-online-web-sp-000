6cbdf3f38ee3f347af9f4e33b594dd461ed18c7b
Micah McCallum <micahmccallum@outlook.com>
2020-03-28 22:57:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => { :name, :age },
+      :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
       },
     :capulet => {
-      :patriarch => { :name, :age },
+      :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
